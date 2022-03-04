ef17bd1a743ca997e41cde9f58242de8cb00c7b3
Rohit <rohitkumbhar3456@gmail.com>
2022-03-04 19:34:41 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import '../styles/App.css';
 
@@ -6,10 +6,28 @@ import '../styles/App.css';
 const searchArray = ["Newton", "School", "Newton School", "React", "Preact", "Node", "Mongoose", "Angular", "Vue", "Apple", "Microsoft", "Netflix", "Meta"]
 const App = () => {
 
+  const [input , setInput] = useState("");
+
+
+
+  const show = () =>{
+    if(input.value){
+      return searchArray.map((item)=>console.log(item))
+    }
+  }
   
   return (
     <div id="main">
-      
+      <input type="text" id='search-input' onChange={show}/>
+      <ul onChange={(e)=>{
+        setInput(e.target.value)
+      }}>
+      {searchArray.map((item, index)=>{
+        return (
+          <li key={index} value={input}>{item}</li>
+        )
+      })}
+      </ul>
     </div>
   )
 }
