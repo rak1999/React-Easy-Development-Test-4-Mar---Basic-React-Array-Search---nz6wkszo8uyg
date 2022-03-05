b70bb4d6cb7e61709cb0fa0f7ac8e25c79d9ebd5
Rohit <rohitkumbhar3456@gmail.com>
2022-03-05 17:49:46 +0530

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -12,13 +12,13 @@ const App = () => {
     <div id="main">
       <input type="text" value={input} id='search-input' onChange={e=>setInput(e.target.value)}/>
       <ul>
-      {searchArray.filter(name => name.match(new RegExp(input , "i")))
-      .map(name=>{
-        return (
-            <li key={name}>{name}</li>
-        )
-      })
-      }
+        {input? searchArray.filter(name => name.match(new RegExp(input , "i")))
+        .map(name=>{
+          return (
+              <li key={name}>{name}</li>
+          )
+        })
+        : ""}
       </ul>
     </div>
   )
